a6a66d0f52b175e2739bf68f7ddab050a626571c
GohWanXuan <54830082+GohWanXuan@users.noreply.github.com>
2020-03-12 19:23:44 +0800

    Updated user rent scooter scenario
    Http call from booking to scooter

--- booking.py
+++ booking.py
@@ -2,12 +2,20 @@ from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 from os import environ
+import requests
+import json
+import sys
+import os
+import pika
+
+scooterURL = "http://localhost:5000/scooter/"
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root@localhost:3306/booking'
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:root@localhost:3306/booking'
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root@localhost:3306/booking'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:root@localhost:3306/booking'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 
 db = SQLAlchemy(app)
 CORS(app)
@@ -16,18 +24,20 @@ class Booking(db.Model):
     __tablename__ = 'booking'
 
     bookingID = db.Column(db.String(5), primary_key=True)
+    scooterID = db.Column(db.String(5), nullable=False)
     parkingLotID = db.Column(db.String(5), nullable=False)
     startTime = db.Column(db.DateTime, nullable=False)
     endTime = db.Column(db.DateTime, nullable=False)
 
-    def __init__(self, bookingID, parkingLotID, startTime, endTime):
+    def __init__(self, bookingID, scooterID, parkingLotID, startTime, endTime):
         self.bookingID = bookingID
+        self.scooterID = scooterID
         self.parkingLotID = parkingLotID
         self.startTime = startTime
         self.endTime = endTime
 
     def json(self):
-        return {"bookingID": self.bookingID, "parkingLotID": self.parkingLotID, 
+        return {"bookingID": self.bookingID, "scooterID": self.scooterID, "parkingLotID": self.parkingLotID,
             "startTime": self.startTime, "endTime": self.endTime}
 
 # return a list of all available bookings
@@ -38,24 +48,55 @@ def get_all():
 # create a new booking
 @app.route("/booking/<string:bookingID>", methods=['POST'])
 def create_booking(bookingID):
+    status = 201
+    result = {}
+
+    # retrieve information about order and order items from the request
     if (Booking.query.filter_by(bookingID=bookingID).first()):
-        return jsonify({"message": "A booking with bookingID '{}' already exists.".format(bookingID)}), 400
+        status = 400
+        result = {"status": status, "message": "A booking with bookingID '{}' already exists.".format(bookingID)}
 
-    data = request.get_json()
-    booking = Booking(bookingID, **data)
-    try:
-        db.session.add(booking)
-        db.session.commit()
-    except:
-        return jsonify({"message": "An error occurred creating the booking."}), 500
+    elif status == 201:
+        data = request.get_json()
+        if (len(data) < 1):
+            status = 404
+            result = {"status": status, "message": "Exmpty booking."}
+        else:
+            booking = Booking(bookingID, **data)
+            
+            try:
+                db.session.add(booking)
+                db.session.commit()
+                scooterID = booking.scooterID
+                parkingLotID = booking.parkingLotID
+                update_scooter(scooterID)
+            except Exception as e:
+                status = 500
+                result = {"status": status, "message": "An error occurred when creating the order in DB.", "error": str(e)}
 
-    return jsonify(booking.json()), 201
+            if status == 201:
+                result = {"status": status, "scooterID": scooterID, "parkingLotID": parkingLotID,
+                    "availabilityStatus": 0}
+
+    update_scooter(result)
+    return result
+
+### Update scooter microservice through a broker
+def update_scooter(result):
+    result = json.loads(json.dumps(result, default=str))
+    if "scooterID" in result: 
+        # inform Scooter
+        scooterID = result["scooterID"]
+        requests.put(scooterURL + str(scooterID), json = result, timeout=1)
+        print("Booking status ({:d}) sent to scooter.".format(result["status"]))
+
+    # r = requests.put(scooterURL + str(scooterID), json = bookingJSON)
 
 # update the endTime of a booking, return the info about the updated booking record 
 @app.route("/booking/<string:bookingID>", methods=['PUT'])
 def update_booking(bookingID):
     booking = Booking.query.filter_by(bookingID=bookingID).first()
-    print(booking.json())
+
     if booking:
         data = request.get_json()
         try:
--- scooter.py
+++ scooter.py
@@ -2,10 +2,15 @@ from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 from os import environ
+import requests
+import json
+
+parkingLotURL = "http://localhost:5002/parkinglot/"
 
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:root@localhost:3306/scooter'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root@localhost:3306/scooter'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
@@ -27,9 +32,9 @@ class Scooter(db.Model):
         return {"scooterID": self.scooterID, "parkingLotID": self.parkingLotID, "availabilityStatus": self.availabilityStatus}
 
 # return a list of all available scooters
-# @app.route("/scooter")
-# def get_all(): 
-#     return jsonify({"scooters": [scooter.json() for scooter in Scooter.query.all()]})
+@app.route("/scooter")
+def get_all(): 
+    return jsonify({"scooters": [scooter.json() for scooter in Scooter.query.all()]})
 
 # return information about a scooter
 @app.route("/scooter/<string:scooterID>")
@@ -51,24 +56,43 @@ def find_by_parkingLotID(parkingLotID):
 # update the parkingLotID and availabilityStatus of a scooter, return the info about the updated scooter record 
 @app.route("/scooter/<string:scooterID>", methods=['PUT'])
 def update_scooter(scooterID):
-    scooter = Scooter.query.filter_by(scooterID=scooterID).first()
-    if scooter:
-        data = request.get_json()
+    result = None
+    if request.is_json:
+        result = request.get_json()
+    else:
+        result = request.get_data()
+        print("Received an invalid scooter update error:")
+        print(result)
+        replymessage = json.dumps({"message": "Scooter update error should be in JSON", "data": result}, default=str)
+        return replymessage, 400 # Bad Request
 
-        try:
-            if scooter.availabilityStatus == data['availabilityStatus']:
-                return jsonify({"message": "An error occurred updating the scooter."}), 500
-            else:
-                scooter.parkingLotID = data['parkingLotID']
-                scooter.availabilityStatus = data['availabilityStatus']
+    status = result["status"]
+    scooterID = result["scooterID"]
+    availabilityStatus = result["availabilityStatus"]
+    parkingLotID = result['parkingLotID']
+
+    if (not(Scooter.query.filter_by(scooterID=scooterID).first())):
+        status = 400
+        result = {"status": status, "message": "A scooter with scooterID '{}' does not exists.".format(scooterID)}
+
+    elif status == 201:
+        dbScooter = Scooter.query.filter_by(scooterID=scooterID).first()
+        if (dbScooter.availabilityStatus == availabilityStatus or dbScooter.parkingLotID == "null"):
+            status = 400
+            result = {"status": status, "message": "A scooter with scooterID '{}' is unavailable to rent.".format(scooterID)}
+        else:
+            try:
+                dbScooter.parkingLotID = "null"
+                dbScooter.availabilityStatus = 0
                 db.session.commit()
+            except Exception as e:
+                status = 500
+                result = {"status": status, "message": "An error occurred when updating the scooter in DB.", "error": str(e)}
 
-        except:
-            return jsonify({"message": "An error occurred updating the scooter."}), 500
-        
-        return jsonify(scooter.json()), 201
-        
-    return jsonify({"message": "Scooter not found."}), 404
+        if status == 201:
+            result = {"status": status, "scooterID": scooterID}
+            print(result)
+    return result
 
 if __name__ == '__main__': 
     app.run(host='0.0.0.0', port=5000, debug=True)
